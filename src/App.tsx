5c68b957b76c32e61b495c71188e0a2dc510fc29
Simeon Akanbi <simeonakanbi99@gmail.com>
2025-08-01 23:15:41 +0100

    feat: add input fields and button for wallet address and request amount

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
-import { Box } from "@chakra-ui/react"
+import { Box, Button, Input } from "@chakra-ui/react"
 
 const App = () => {
   return (
     <Box>
-      
+      <Box>
+        <p>Devnet Faucet</p>
+      </Box>
+      <Box>
+        <Input
+          placeholder="Enter your wallet address"
+        />
+        <Input
+          placeholder="Enter the amount to request"
+          type="number"
+          min="0"
+        />
+        <Button>Request</Button>
+      </Box>
     </Box>
   )
 }
